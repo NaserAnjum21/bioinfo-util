8ee05db11d06ffdea4e31116f587fb37c5426f5e
Naser Anjum <na.samudro2196@gmail.com>
2021-05-26 01:18:06 +0600

    Intial commit

---
+++ fasta_separator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while read line
+do
+    if [[ ${line:0:1} == '>' ]]
+    then
+        outfile=${line#>}.fa
+        echo $line > $outfile
+    else
+        echo $line >> $outfile
+    fi
+done < $1
